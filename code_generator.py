9ceed2f0df12fdea55dced90d94b22f326c29d0a
tanishkmalviya22 <165447615+Tanishk022@users.noreply.github.com>
2025-10-03 15:18:45 +0530

    Temp_ode_generator.py

--- code_generator.py
+++ code_generator.py
@@ -85,170 +85,71 @@
 #             st.error(f"❌ Error: {e}")
 
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import streamlit as st
 import pickle
+import numpy as np
+
+# Load trained model
+model = pickle.load(open("regression_model_2.pkl", "rb"))
+if st.button("Information about AQI Range"):
+        st.image("Screenshot 2025-10-03 113142.png")
+col1,col2 = st.columns([2,2])
+with col1:
+    st.image("https://medicaldialogues.in/wp-content/uploads/2016/11/AIR-POLLUTION.jpg")
+with col2:
+    st.title("Air Quality Prediction App")
+
+# st.image("Screenshot 2025-10-03 113142.png")
+with st.sidebar:
+# User inputs
+    # City = st.text_input("City")fil
+    PM10  = st.number_input("Enter PM10", min_value=0.0,max_value=50.0, step=1.0)
+    PM2_5 = st.number_input("Enter PM2.5", min_value=0.0,max_value=30.0, step=1.0)
+    NO2   = st.number_input("Enter NO2", min_value=0.0,max_value=40.0, step=1.0)
+    O3    = st.number_input("Enter O3", min_value=0.0,max_value=50.0, step=1.0)
+    CO    = st.number_input("Enter CO", min_value=0.0,max_value=1.0, step=1.0)
+    SO2   = st.number_input("Enter SO2", min_value=0.0,max_value=40.0, step=1.0)
+    NH3   = st.number_input("Enter NH3", min_value=0.0,max_value=200.0, step=1.0)
+    # AQI  = st.number_input("Enter AQI", min_value=0.0, step=1)
+
+if st.button("Predict Air Quality"):
+    # 'PM2.5', 'PM10', 'NO', 'NO2', 'NOx', 'NH3', 'CO', 'SO2', 'O3',
+    #    'Benzene', 'Toluene'
+    # features = np.array([[PM2.5, PM10, NO, NO2, NOx, NH3, CO, SO2, O3, Benzene, Toluene]])
+
+    features = np.array([[PM10,PM2_5, NO2, O3, CO, SO2,NH3]])
+
+
+    # features = np.array([[T, TM, TT, SLP, H, VV, V, VM]])
+    y_pred = model.predict(features)[0]
+    # prediction = np.clip(prediction, 0, 500)
+
+
+    st.subheader(f"Predicted AQI: {round(y_pred,2)}")
+
+    # Easy explanation block
+    st.subheader("Leval of Concern")
+    if y_pred >= 0 and y_pred <= 51:
+        st.subheader("Good Air Quality")
+        st.success("There are negligible levels of pollutants like particulate matter and ground-level ozone, the air is fresh and crisp, and you can breathe easily without any respiratory discomfort.")
+
+    elif y_pred >= 51 and y_pred <= 100:
+        st.subheader("Moderate Air Quality")
+        st.info("Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution..")
+        
+    elif y_pred >= 101 and y_pred <= 150:
+        st.subheader("Unhealthy for Sensitive Groups")
+        st.warning("Members of sensitive groups may experience health effects. The general public is less likely to be affected.")
+    elif y_pred >= 151 and y_pred <= 200:
+        st.subheader("Unhealthy")
+        st.warning("Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.")
+    elif y_pred >= 201 and y_pred <= 300:
+        st.subheader("Very Unhealthy")
+        st.warning("Health alert –The risk of health effects is increased for everyone.")
+    else:
+        st.write("Hazardous")
+        st.error("– Serious health risks, stay indoors and avoid exposure.]")
 
-df=pd.read_csv("city_day.csv")
 
-print(df.head())
-
-print(df.info())
-
-print(df.describe())
-
-print(df.isnull().sum())
-
-
-# df=df.drop('Date',axis=1)
-df=df.drop('NO',axis=1)
-df=df.drop('NOx',axis=1)
-df=df.drop('Benzene',axis=1)
-df=df.drop('Toluene',axis=1)
-df=df.drop('Xylene',axis=1)
-df=df.drop('AQI_Bucket',axis=1)
-
-print(df.shape)
-
-print(df["AQI"].value_counts())
-
-# print("CHECK_POINT1")
-df=df.drop('Date',axis=1)
-
-
-sns.heatmap(df.isnull(),yticklabels=False,cbar=True,cmap='viridis')
-plt.show()
-
-# print("CHECK_POINT2")
-
-#pairplot which show reletionship
-sns.pairplot(df)
-plt.show()
-
-# print("CHECK_POINT3")
-
-#correlation heatmap
-plt.figure(figsize=(8,8))
-sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm")
-plt.show()
-
-# print("CHECK_POINT4")
-
-# EDA Handling Missing Values
-
-df.fillna({
-    "PM2.5": df["PM2.5"].mean(),
-    "PM10": df["PM10"].mean(),
-    "NO2":df["NO2"].mean(),
-    "NH3": df["NH3"].mean(),
-    "CO":df["CO"].mean(),
-    "SO2":df["SO2"].mean(),
-    "O3": df["O3"].mean(),
-    "AQI":df["AQI"].mean()
-}, inplace=True)
-
-
-# print("CHECK_POINT5")
-
-
-# Dividing the Data into X and Y
-
-x=df.iloc[:,1:8].values #independent features
-y=df.iloc[:,-1].values # dependent features
-
-
-# print("CHECK_POINT6")
-
-print(x)
-print(y)
-
-# print("CHECK_POINT7")
-
-
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test= train_test_split(x,y,test_size=0.3,random_state=0)
-
-# print("CHECK_POINT8")
-
-
-from sklearn.linear_model import LinearRegression
-linreg=LinearRegression()
-linreg.fit(x_train,y_train)
-
-
-# print("CHECK_POINT9")
-
-print(linreg.coef_)
-
-print(linreg.intercept_)
-
-# print("CHECK_POINT10")
-
-
-y_pred=linreg.predict(x_test)
-y_pred
-
-print("CHECK_POINT11")
-
-y_pred.shape
-
-
-# model evaluation
-
-x=df.iloc[:,1:8]
-y=df.iloc[:,-1]
-
-
-# print("CHECK_POINT12")
-
-print(x.columns)
-print(y)
-
-coef_df=pd.DataFrame(linreg.coef_,x.columns,columns=["Coefficient"])
-coef_df
-
-plt.scatter(y_test,y_pred)
-plt.show()
-
-# print("CHECK_POINT13")
-
-
-# prediction already hai X_test ke liye
-y_pred = linreg.predict(x_test)
-# Scatter Plot
-
-plt.figure(figsize=(6.2,4.5))
-plt.scatter(y_test, y_pred, color='blue', alpha=0.6)
-plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # diagonal line
-plt.xlabel('Actual AQI')
-plt.ylabel('Predicted AQI')
-plt.title('Actual vs Predicted AQI')
-plt.show()
-
-sns.distplot((y_test-y_pred),bins=50)
-plt.show()
-
-# print("CHECK_POINT14")
-
-
-from sklearn import metrics
-
-print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
-print('MSE:', metrics.mean_squared_error(y_test, y_pred))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-
-
-# open a file, where you ant to store the data
-file = open('regression_model_2.pkl', 'wb')
-# dump information to that file
-pickle.dump(linreg, file)
-
-
-print("file is safe")
-
-
-# print(y.head())
 
 
